16833b9764e29da66c2a9200249bf16fc90c21d7
Aly Margaret Rodger <arodger@node309.cluster>
2024-09-26 17:13:58 -0400

    PCA in ggplot

--- population_genomics/docs/03_PCA_Admixture.r
+++ population_genomics/docs/03_PCA_Admixture.r
@@ -3,6 +3,8 @@ library(tidyverse)
 library(vcfR)
 library(SNPfiltR)
 
+options(bitmapType = "cairo")
+
 # New library for PCA
 library(LEA)
 
@@ -38,9 +40,27 @@ geno <- vcf2geno(input.file = "/gpfs1/home/a/r/arodger/vcf_final.filtered.thinne
                  output.file = "outputs/vcf_final.filtered.thinned.vcf.geno")
 CentPCA <- LEA::pca("outputs/vcf_final.filtered.thinned.vcf.geno", scale=TRUE)
 
+# To reload a previous PCA
+CentPCA <- load.pcaProject("vcf_final.filtered.thinned.vcf.pcaProject")
+
 # Using base R
 # Porjections is first 2 pc axes
-plot(CentPCA$projections,
-     col=as.factor(meta2$region))
-legend("bottomright", legend=as.factor(unique(meta2$region)),
-       fill=as.factor(unique(meta2$region)))
+#plot(CentPCA$projections,
+     #col=as.factor(meta2$region))
+#legend("bottomright", legend=as.factor(unique(meta2$region)),
+      # fill=as.factor(unique(meta2$region)))
+
+show(CentPCA)
+plot(CentPCA)
+
+ggplot(as.data.frame(CentPCA$projections),
+       aes(x=V1, y=V2, color=meta2$region, shape=meta2$continent)) +
+       geom_point(alpha=0.5) +
+  labs(title = "Centaurea genetic PCA", x="PC1",y="PC2", color="Region", shape="Continent")
+
+ggsave("figures/CentPCA_PC1vPC2.pdf")
+
+# percentage shown on PCA graphs is the eigen vector divided by the sum of all eigenvectors
+
+
+# Admixture Analysis
